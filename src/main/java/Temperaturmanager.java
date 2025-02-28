b7c49d4d5c3321db1b484cd7788fb68652e71fec
iminzu1104 <ioanalexandru110@yahoo.com>
2025-02-28 09:12:40 +0100

    commited

--- src/main/java/Temperaturmanager.java
+++ src/main/java/Temperaturmanager.java
@@ -1,6 +1,13 @@
-public class Temperaturmanager {
-    public void regeleTemperatur(){
+import java.util.List;
+import java.util.ArrayList;
 
+public class Temperaturmanager {
+
+    private List<Raum> reaume = new ArrayList<>();
+
+    public void regeleTemperatur(){
+        for(Raum raum : reaume){}
+            reaume = new ArrayList<>();
     }
 
     public void erstelleRaeume(){
