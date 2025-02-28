a95cbebc26e090e72053dbb6d5de30739d352b25
iminzu1104 <ioanalexandru110@yahoo.com>
2025-02-28 09:09:09 +0100

    commited

--- src/main/java/Raum.java
+++ src/main/java/Raum.java
@@ -1,18 +1,33 @@
+import java.util.List;
+import java.util.ArrayList;
+
 class Raum {
     private int raumId;
     private int zieltemperatur;
+
+    private List<Fenster> fensterListe = new ArrayList<>();
+
+    private List<Heizung> heizungListe = new ArrayList<>();
+
+    private Thermometer;
+
 
     public Raum(int raumId, int zieltemperatur) {
         this.raumId = raumId;
         this.zieltemperatur = zieltemperatur;
     }
 
-    public boolean hatOffeneFenster(){
-
+    public boolean hatOffeneFenster() {
+        for (Fenster fenster : fensterListe) {
+            if (fenster.isOffen()) {
+                return true;
+            }
+        }
+        return false;
     }
 
     public void regeleTemperatur(){
-
+        if
     }
 
     public List <Fenster> getFensterliste()
